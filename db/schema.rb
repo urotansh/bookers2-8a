fe1a31700b4d7d68b4ab50f337708a2c8ce65661
urotansh <114655455+urotansh@users.noreply.github.com>
2022-11-03 15:12:43 +0900

    [Update] テーブル名変更(chats->messages)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_02_213852) do
+ActiveRecord::Schema.define(version: 2022_11_02_230123) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,14 +56,6 @@ ActiveRecord::Schema.define(version: 2022_11_02_213852) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "chats", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "room_id"
-    t.string "message"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
@@ -72,9 +64,9 @@ ActiveRecord::Schema.define(version: 2022_11_02_213852) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.integer "sender_id"
-    t.integer "recipient_id"
-    t.text "body"
+    t.integer "user_id"
+    t.integer "room_id"
+    t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
