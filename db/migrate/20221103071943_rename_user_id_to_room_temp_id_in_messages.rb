b9be94f98bd1f8abb7d2bc4a637dd483f086fea8
urotansh <114655455+urotansh@users.noreply.github.com>
2022-11-03 16:48:26 +0900

    [Revert] messages/room_usersテーブルのカラム名変更取り消し

--- db/migrate/20221103071943_rename_user_id_to_room_temp_id_in_messages.rb
+++
@@ -1,6 +0,0 @@
-class RenameUserIdToRoomTempIdInMessages < ActiveRecord::Migration[6.1]
-  def up
-    rename_column :messages, :user_id, :room_temp_id
-    rename_column :messages, :room_id, :user_temp_id
-  end
-end
--- db/migrate/20221103072402_rename_user_id_to_room_temp_id_in_room_users.rb
+++
@@ -1,6 +0,0 @@
-class RenameUserIdToRoomTempIdInRoomUsers < ActiveRecord::Migration[6.1]
-  def up
-    rename_column :room_users, :user_id, :room_temp_id
-    rename_column :room_users, :room_id, :user_temp_id
-  end
-end
