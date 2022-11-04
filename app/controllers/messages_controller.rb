ce5b33797149b106dc4a60b1483251cf33c7500b
urotansh <114655455+urotansh@users.noreply.github.com>
2022-11-04 20:22:00 +0900

    [Add] messages#showへRoom/RoomUserの作成処理(初回のみ)を定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,25 @@
 class MessagesController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    
+    # ログインユーザが所属するroomのroom_id一覧を配列で取得
+    current_user_rooms = current_user.room_users.pluck(:room_id)
+    # 相手ユーザが所属するroomのroom_id一覧を配列で取得
+    user_rooms = @user.room_users.pluck(:room_id)
+    # 両ユーザが所属するroomのroom_idを配列で取得
+    room_id = current_user_rooms & user_rooms
+    
+    unless room_id.empty?
+      # 両ユーザが所属するroomが存在する場合はroomをオブジェクトで取得
+      @room = Room.find_by(id: room_id)
+    else
+      # 両ユーザが所属するroomが存在しない場合はroomとroom_userを作成
+      @room = Room.new
+      @room.save
+      RoomUser.create(user_id: current_user.id, room_id: @room.id)
+      RoomUser.create(user_id: @user.id, room_id: @room.id)
+    end
+    
   end
   
   def create
